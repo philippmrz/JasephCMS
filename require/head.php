3904db91942737a76c51801de0b61b155757fb34
Philipp Merz <phmrch@gmail.com>
2018-02-13 21:59:22 +0100

    Added new fonts

--- require/head.php
+++ require/head.php
@@ -6,5 +6,6 @@ if ($_COOKIE["theme"] == "hacker") {
     echo '<link rel="stylesheet" href="style/normal.css" id="pagestyle"/>';
 }
 ?>
+<link href="https://fonts.googleapis.com/css?family=Lato|Slabo+27px" rel="stylesheet">
 <script src="script/script.js"></script>
 <title>JasephCMS</title>
