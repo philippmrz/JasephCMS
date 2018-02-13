7bb79bf9c299c26f72607fe646eeb80e211a125c
Philipp Merz <phmrch@gmail.com>
2018-02-13 21:54:44 +0100

    Changed page titles

--- require/head.php
+++ require/head.php
@@ -1,10 +1,10 @@
 <link rel="icon" href="assets/icon_0.png"/>
 <?php
-if($_COOKIE["theme"] == "hacker") {
-  echo '<link rel="stylesheet" href="style/hacker.css" id="pagestyle"/>';
+if ($_COOKIE["theme"] == "hacker") {
+    echo '<link rel="stylesheet" href="style/hacker.css" id="pagestyle"/>';
 } else {
-  echo '<link rel="stylesheet" href="style/normal.css" id="pagestyle"/>';
+    echo '<link rel="stylesheet" href="style/normal.css" id="pagestyle"/>';
 }
 ?>
 <script src="script/script.js"></script>
-<title>jaseph</title>
+<title>JasephCMS</title>
