b532db41ba959082d1af4f37af509f224272d4df
Philipp Merz <phmrch@gmail.com>
2018-02-13 22:49:20 +0100

    Made pages more responsive

--- require/head.php
+++ require/head.php
@@ -1,4 +1,5 @@
 <link rel="icon" href="assets/icon_0.png"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <?php
 if ($_COOKIE["theme"] == "hacker") {
     echo '<link rel="stylesheet" href="style/hacker.css" id="pagestyle"/>';
