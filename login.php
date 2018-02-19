6a724ad14a96a9416449c4f9a330e30b8cb39b77
Zaby <32384332+Zaby0@users.noreply.github.com>
2018-02-19 11:16:14 +0100

    Finished hash and login / register framework

--- login.php
+++ login.php
@@ -1,14 +1,86 @@
 <!doctype html>
-<html>
-<head>
-  <?php require('require/head.php');?>
-</head>
-<body>
-  <?php require('require/header.php');?>
-  <div id="content">
-  </div>
-  <div id="footer">
-    <a href="https://github.com/phmrz/JasephCMS" title="Check out the main branch of this page on GitHub!">Created as a school project by Jakob Mainka, Philipp Merz and Sebastian Scheinert</a>
-  </div>
+<html lang="de">
+  <head>
+  <title>Jaseph</title>
+  </head>
+  <body>
+
+<?php
+$server = "localhost";
+$user = "root";
+$pw = "";
+$db = "data";
+$msg = array();
+
+$connect = new mysqli($server,$user,$pw,$db);
+
+if($connect->connect_error){
+  die("Connecting to MySQL or database failed:<b><i> ". $connect->connect_error . "</b></i>");
+}
+
+if(isset($_POST["logbtn"])){
+  $uname = $_POST["uname"];
+  $pword = $_POST["pword"];
+  if(!empty($uname) && !empty($pword)){
+	  
+	$result = $connect->query("SELECT PASSWORD,USERNAME FROM user WHERE USERNAME='$uname'");
+	$row = $result->fetch_assoc();
+	$db_p = $row["PASSWORD"];
+    if($result){
+      if(password_verify($pword,$db_p)){
+        //pass
+      header("location:"/*mainpage*/);
+        exit;
+      }
+      else{
+        //invalid
+        array_push($msg,"invalid password or username");
+      }
+    }
+    else{
+      echo"query error";
+    }
+  }
+  else{
+    array_push($msg,"please enter your username and your password");
+  }
+}
+$connect->close();
+?>
+<center>
+<h3>Login</h3>
+<form method="POST" action="">
+<table border=1>
+	<tr>
+		<td>Username:  </td>
+		<td>
+      <?php
+      if(isset($uname) && !empty($uname)){
+        echo "<input type=\"text\" name=\"uname\"value=\"" . $uname ."\"/></td>";
+      }
+      else{
+        echo "<input type=\"text\" name=\"uname\"/></td>";
+      }
+    ?>
+    </td>
+	</tr>
+  <tr>
+    <td>Password: </td>
+    <td><input type="password" name="pword"/></td>
+  </tr>
+</table></br>
+<input type="submit" name="logbtn" value="Do it"/></br>
+<?php
+if(!empty($msg)){
+  foreach($msg as $text){
+    echo "<font size=\"2px\" color=\"red\"><i>" .$text . "</font></i><br>";
+  }
+}
+?>
+<h6>No Account yet? Click <a href="register.php">here</a> to register</h6>
+
+</form>
+</center>
+
 </body>
 </html>
--- register.php
+++ register.php
@@ -1,21 +1,96 @@
 <!doctype html>
-<html>
-<head>
-  <?php require('require/head.php');?>
-</head>
-<body>
-  <?php require('require/header.php');?>
-  <div class="content">
-    <form action="registered" method="POST">
-      <input name="username" type="text" placeholder="Username"/><br>
-      <input name="password" type="password" placeholder="Password"/><br>
-      <input name="repeat" type="password" placeholder="Repeat Password"/><br>
-      <input name="email" type="text" placeholder="E-Mail (Optional)"/><br>
-      <button type="submit">Register</button><br>
-    </form>
-  </div>
-  <div id="footer">
-    <a href="https://github.com/phmrz/JasephCMS" title="Check out the main branch of this page on GitHub!">Created as a school project by Jakob Mainka, Philipp Merz and Sebastian Scheinert</a>
-  </div>
+<html lang="de">
+  <head>
+  <title>Jaseph</title>
+  </head>
+  <body>
+
+<?php
+$server = "localhost";
+$user = "root";
+$pw = "";
+$db = "data";
+$msg = array();
+
+$connect = new mysqli($server,$user,$pw,$db);
+
+if($connect->connect_error){
+  die("Connecting to MySQL or database failed:<b><i> ". $connect->connect_error . "</b></i>");
+}
+
+if(isset($_POST["regbtn"])){
+  $uname = $_POST["uname"];
+  $pword = $_POST["pword"];
+  $pwordval = $_POST["pwordval"];
+
+  if(empty($uname)){
+    array_push($msg,"please enter a username");
+  }
+
+  if(empty($pword)){
+    array_push($msg,"please enter a password");
+  }
+  $result = $connect->query("SELECT * FROM user WHERE USERNAME='$uname'");
+  if($result->num_rows > 0){
+    array_push($msg,"username already exists");
+  }
+
+  if($pword != $pwordval){
+    array_push($msg,"passwords must match");
+  }
+
+  if(count($msg) == 0){
+    $pword = password_hash($pword,PASSWORD_DEFAULT);
+    $result = $connect->query("INSERT INTO user (USERNAME,PASSWORD)
+            VALUES ('$uname','$pword')");
+    if($result){
+      header("Location: login.php");
+      exit;
+    }
+    else{
+      echo"query error";
+    }
+  }
+
+}
+$connect->close();
+?>
+
+<center>
+  <h2>Register</h2>
+  <form method="POST" action="">
+  <table border=1>
+  	<tr>
+  		<td>Username:  </td>
+  		<td>
+        <?php
+        if(isset($uname) && !empty($uname)){
+          echo "<input type=\"text\" name=\"uname\"value=\"" . $uname ."\"/></td>";
+        }
+        else{
+          echo "<input type=\"text\" name=\"uname\"/></td>";
+        }
+      ?>
+  	</tr>
+    <tr>
+      <td>Password: </td>
+      <td><input type="password" name="pword"/></td>
+    </tr>
+    <tr>
+      <td>Password(again): </td>
+      <td><input type="password" name="pwordval"/></td>
+    </tr>
+  </table></br>
+  <input type="submit" name="regbtn" value="Register"/></br>
+</form>
+<?php
+if(!empty($msg)){
+  foreach($msg as $text){
+    echo "<font size=\"2px\" color=\"red\"><i>" .$text . "</font></i><br>";
+  }
+}
+?>
+</center>
+
 </body>
 </html>
