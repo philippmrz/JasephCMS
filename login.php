aa5fe6e4069f94d033024d1b7a6fa68e63f45a93
Philipp Merz <phmrch@gmail.com>
2018-02-22 17:33:01 +0100

    Replaced hardcoded credentials with credentials.php

--- login.php
+++ login.php
@@ -6,44 +6,36 @@
   <body>
 
 <?php
-$server = "localhost";
-$user = "root";
-$pw = "";
-$db = "data";
-$msg = array();
+require 'require/credentials.php';
+$connect = new mysqli($servername, $username, $password, $dbname);
 
-$connect = new mysqli($server,$user,$pw,$db);
-
-if($connect->connect_error){
-  die("Connecting to MySQL or database failed:<b><i> ". $connect->connect_error . "</b></i>");
+if ($connect->connect_error) {
+    die("Connecting to MySQL or database failed:<b><i> " . $connect->connect_error . "</b></i>");
 }
 
-if(isset($_POST["logbtn"])){
-  $uname = $_POST["uname"];
-  $pword = $_POST["pword"];
-  if(!empty($uname) && !empty($pword)){
-	  
-	$result = $connect->query("SELECT PASSWORD,USERNAME FROM user WHERE USERNAME='$uname'");
-	$row = $result->fetch_assoc();
-	$db_p = $row["PASSWORD"];
-    if($result){
-      if(password_verify($pword,$db_p)){
-        //pass
-      header("location:"/*mainpage*/);
-        exit;
-      }
-      else{
-        //invalid
-        array_push($msg,"invalid password or username");
-      }
+if (isset($_POST["logbtn"])) {
+    $uname = $_POST["uname"];
+    $pword = $_POST["pword"];
+    if (!empty($uname) && !empty($pword)) {
+
+        $result = $connect->query("SELECT PASSWORD,USERNAME FROM user WHERE USERNAME='$uname'");
+        $row = $result->fetch_assoc();
+        $db_p = $row["PASSWORD"];
+        if ($result) {
+            if (password_verify($pword, $db_p)) {
+                //pass
+                header("location:" /*mainpage*/);
+                exit;
+            } else {
+                //invalid
+                array_push($msg, "invalid password or username");
+            }
+        } else {
+            echo "query error";
+        }
+    } else {
+        array_push($msg, "please enter your username and your password");
     }
-    else{
-      echo"query error";
-    }
-  }
-  else{
-    array_push($msg,"please enter your username and your password");
-  }
 }
 $connect->close();
 ?>
@@ -55,13 +47,12 @@ $connect->close();
 		<td>Username:  </td>
 		<td>
       <?php
-      if(isset($uname) && !empty($uname)){
-        echo "<input type=\"text\" name=\"uname\"value=\"" . $uname ."\"/></td>";
-      }
-      else{
-        echo "<input type=\"text\" name=\"uname\"/></td>";
-      }
-    ?>
+if (isset($uname) && !empty($uname)) {
+    echo "<input type=\"text\" name=\"uname\"value=\"" . $uname . "\"/></td>";
+} else {
+    echo "<input type=\"text\" name=\"uname\"/></td>";
+}
+?>
     </td>
 	</tr>
   <tr>
@@ -71,10 +62,10 @@ $connect->close();
 </table></br>
 <input type="submit" name="logbtn" value="Do it"/></br>
 <?php
-if(!empty($msg)){
-  foreach($msg as $text){
-    echo "<font size=\"2px\" color=\"red\"><i>" .$text . "</font></i><br>";
-  }
+if (!empty($msg)) {
+    foreach ($msg as $text) {
+        echo "<font size=\"2px\" color=\"red\"><i>" . $text . "</font></i><br>";
+    }
 }
 ?>
 <h6>No Account yet? Click <a href="register.php">here</a> to register</h6>
--- register.php
+++ register.php
@@ -6,51 +6,47 @@
   <body>
 
 <?php
-$server = "localhost";
-$user = "root";
-$pw = "";
-$db = "data";
+require 'require/credentials.php';
 $msg = array();
 
-$connect = new mysqli($server,$user,$pw,$db);
+$connect = new mysqli($servername, $username, $password, $dbname);
 
-if($connect->connect_error){
-  die("Connecting to MySQL or database failed:<b><i> ". $connect->connect_error . "</b></i>");
+if ($connect->connect_error) {
+    die("Connecting to MySQL or database failed:<b><i> " . $connect->connect_error . "</b></i>");
 }
 
-if(isset($_POST["regbtn"])){
-  $uname = $_POST["uname"];
-  $pword = $_POST["pword"];
-  $pwordval = $_POST["pwordval"];
+if (isset($_POST["regbtn"])) {
+    $uname = $_POST["uname"];
+    $pword = $_POST["pword"];
+    $pwordval = $_POST["pwordval"];
 
-  if(empty($uname)){
-    array_push($msg,"please enter a username");
-  }
+    if (empty($uname)) {
+        array_push($msg, "please enter a username");
+    }
 
-  if(empty($pword)){
-    array_push($msg,"please enter a password");
-  }
-  $result = $connect->query("SELECT * FROM user WHERE USERNAME='$uname'");
-  if($result->num_rows > 0){
-    array_push($msg,"username already exists");
-  }
+    if (empty($pword)) {
+        array_push($msg, "please enter a password");
+    }
+    $result = $connect->query("SELECT * FROM user WHERE USERNAME='$uname'");
+    if ($result->num_rows > 0) {
+        array_push($msg, "username already exists");
+    }
 
-  if($pword != $pwordval){
-    array_push($msg,"passwords must match");
-  }
+    if ($pword != $pwordval) {
+        array_push($msg, "passwords must match");
+    }
 
-  if(count($msg) == 0){
-    $pword = password_hash($pword,PASSWORD_DEFAULT);
-    $result = $connect->query("INSERT INTO user (USERNAME,PASSWORD)
+    if (count($msg) == 0) {
+        $pword = password_hash($pword, PASSWORD_DEFAULT);
+        $result = $connect->query("INSERT INTO user (USERNAME,PASSWORD)
             VALUES ('$uname','$pword')");
-    if($result){
-      header("Location: login.php");
-      exit;
+        if ($result) {
+            header("Location: login.php");
+            exit;
+        } else {
+            echo "query error";
+        }
     }
-    else{
-      echo"query error";
-    }
-  }
 
 }
 $connect->close();
@@ -64,13 +60,12 @@ $connect->close();
   		<td>Username:  </td>
   		<td>
         <?php
-        if(isset($uname) && !empty($uname)){
-          echo "<input type=\"text\" name=\"uname\"value=\"" . $uname ."\"/></td>";
-        }
-        else{
-          echo "<input type=\"text\" name=\"uname\"/></td>";
-        }
-      ?>
+if (isset($uname) && !empty($uname)) {
+    echo "<input type=\"text\" name=\"uname\"value=\"" . $uname . "\"/></td>";
+} else {
+    echo "<input type=\"text\" name=\"uname\"/></td>";
+}
+?>
   	</tr>
     <tr>
       <td>Password: </td>
@@ -84,10 +79,10 @@ $connect->close();
   <input type="submit" name="regbtn" value="Register"/></br>
 </form>
 <?php
-if(!empty($msg)){
-  foreach($msg as $text){
-    echo "<font size=\"2px\" color=\"red\"><i>" .$text . "</font></i><br>";
-  }
+if (!empty($msg)) {
+    foreach ($msg as $text) {
+        echo "<font size=\"2px\" color=\"red\"><i>" . $text . "</font></i><br>";
+    }
 }
 ?>
 </center>
