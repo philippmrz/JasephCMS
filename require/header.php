c7e2747bb38d4715f9ee7e9bb5893f9e33029eef
Philipp Merz <phmrch@gmail.com>
2018-03-07 21:21:47 +0100

    Login and Register button now only show when not logged in

--- require/header.php
+++ require/header.php
@@ -1,8 +1,10 @@
 <div id="head-wrap">
     <a href="index"><img id="head-logo" src="assets/jaseph_normal.png"></a>
     <div id="btn-holder">
+    <?php if(!isset($_COOKIE["logcheck"])):?>
         <a href="login"><button id="login" class="btn">login</button></a>
         <a href="register"><button id="register" class="btn">register</button></a>
+<?php endif; ?>
         <img id="mask" src="assets/mask.png" onclick="swapStyle()">
     </div>
 </div>
