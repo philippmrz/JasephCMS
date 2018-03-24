a665845106e9489506bed35428d432adae6ff214
Philipp Merz <phmrch@gmail.com>
2018-03-25 00:36:57 +0100

    Design Overhaul (Finished index, other pages still unfinished)

--- index.php
+++ index.php
@@ -2,98 +2,69 @@
 <html>
 <head>
   <?php require 'require/head.php';?>
+  <link rel="stylesheet" href="style/index.css" id="pagestyle">
 </head>
 <body>
-  <?php require 'require/header.php';?>
-  <a href="#" onclick="window.scrollTo(0,0);" title="Go to top">
-    <img id="floating-button-gotop" src="assets/gotop-button.png" width="50px"/>
+
+<?php if (isset($_COOKIE["logcheck"])): ?>
+  <a href="newpost" title="Create a new post">
+    <img id="floating-button-newpost" src="assets/newpost-button.png"/>
   </a>
-  <?php if (isset($_COOKIE["logcheck"])): ?>
-    <a href="newpost" title="Create a new post">
-      <img id="floating-button-newpost" src="assets/newpost-button.png"/>
-    </a>
-  <?php endif; ?>
-  <script>applyStyle();</script>
-  <div class="content">
-<?php
-// Credentials for this server
-require 'require/credentials.php';
+<?php endif; ?>
 
-$mysqli = new mysqli("$servername", "$username", "$password", "$dbname");
+<div id='grid-wrap'>
+  <?php require 'require/header.php';?>
+  <div id='sidebar'></div>
+  <div id='content'>
+    <div class='post'>
+      <img class='thumbnail' src='assets/dummy-thumbnail.png'>
 
-if ($mysqli->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-}
-function random_string($length) {
-    $characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!$.";
-    $c_length = strlen($characters);
-    $string = "";
-    for ($i=0;$i<$length;$i++) {
-        $string .= $characters[rand(0, $c_length -1)];
-    }
-    return $string;
-}
+      <div class='post-without-tn'>
+        <div class='post-info'>
+          <p class='title'>dummy title text long one</p>
+          <div class='date-uname'>
+            <a class='username'>username</a>
+            <p class='at'>at</p>
+            <p class='date'>2018-12-12</p>
+          </div>
+        </div>
+        <p class='post-text'>lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem dolor ipsum sit amet sit dolor ipsum dolor sit dolor amet sit lorem. Lorem ipsum dolor sit amet sit amet dolor impsum alorem ipsum dolor sit amet lorem ipsum dolor sit</p>
+      </div>
+    </div>
+    <div class='post'>
+      <img class='thumbnail' src='assets/dummy-thumbnail.png'>
 
-//if revisit when "stay logged in" was checked before
-if (isset($_COOKIE["identifier"]) && isset($_COOKIE["token"])) {
-    $identifier = $_COOKIE["identifier"];
-    $token = $_COOKIE["token"];
-    $result = $mysqli->query("SELECT TOKEN FROM $usertable WHERE IDENTIFIER='$identifier'");
-    $row = $result->fetch_assoc();
-    $db_token = $row["TOKEN"];
-    $hash_token = hash("sha256", $token);
+      <div class='post-without-tn'>
+        <div class='post-info'>
+          <p class='title'>dummy title text long one</p>
+          <div class='date-uname'>
+            <a class='username'>username</a>
+            <p class='at'>at</p>
+            <p class='date'>2018-12-12</p>
+          </div>
+        </div>
+        <p class='post-text'>lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem dolor ipsum sit amet sit dolor ipsum dolor sit dolor amet sit lorem. Lorem ipsum dolor sit amet sit amet dolor impsum alorem ipsum dolor sit amet lorem ipsum dolor sit</p>
+      </div>
+    </div>
+    <div class='post'>
+      <img class='thumbnail' src='assets/dummy-thumbnail.png'>
 
-    if ($hash_token == $db_token) {//new token
-        $new_token = random_string(32);
-        $hash_new_token = hash("sha256", $new_token);
-        $update = $mysqli->query("UPDATE $usertable SET TOKEN = '$hash_new_token' WHERE IDENTIFIER = '$identifier'");
-        $get_uname = $mysqli->query("SELECT USERNAME FROM $usertable WHERE IDENTIFIER = '$identifier'");
-        if (!$get_uname) {
-            echo $mysqli->error;
-        }
-        $row_u = $get_uname->fetch_assoc();
-        $db_uname = $row_u["USERNAME"];
-        setcookie("identifier", $identifier, time()+86400*356);
-        setcookie("token", $new_token, time()+86400*356);
-        setcookie("logcheck", "true", time()+86400*356);
-        setcookie("uname", $db_uname, time()+86400*356);
-    } else {
-        die("You're a cheater");
-    }
-}
-      $query = "SELECT TITLE, CONTENT, SUBSTRING(DATE, 1, 10) AS DATE, USERNAME FROM $posttable P, $usertable U WHERE P.USERID = U.USERID ORDER BY POSTID DESC";
-
-      //Fetch posts from database and echo them to paragraphs
-      if ($result = $mysqli->query($query)) {
-        while ($row = $result->fetch_assoc()) {
-          echo "<div class='post'>";
-
-          echo "<p class='post-title-date'>";
-
-          echo $row["TITLE"];
-
-          echo "<span class='align-right'>";
-          echo $row["USERNAME"];
-          echo "</span>";
-          echo "<br>";
-          echo "<span class='align-right'>";
-          echo $row["DATE"];
-          echo "</span>";
-
-          echo "</p>";
-
-          echo "<p class='posttext'>";
-          echo nl2br($row["CONTENT"]);
-          echo "</p>";
-
-          echo "</div>";
-        }
-      } else {
-        echo "Couldn't query database, try again";
-      }
-      ?>
+      <div class='post-without-tn'>
+        <div class='post-info'>
+          <p class='title'>dummy title text long one</p>
+          <div class='date-uname'>
+            <a class='username'>username</a>
+            <p class='at'>at</p>
+            <p class='date'>2018-12-12</p>
+          </div>
+        </div>
+        <p class='post-text'>lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem dolor ipsum sit amet sit dolor ipsum dolor sit dolor amet sit lorem. Lorem ipsum dolor sit amet sit amet dolor impsum alorem ipsum dolor sit amet lorem ipsum dolor sit</p>
+      </div>
+    </div>
   </div>
-  <?php require 'require/footer.php';?>
+</div>
+
+<script>applyStyle();</script>
+
 </body>
 </html>
--- require/header.php
+++ require/header.php
@@ -1,13 +1,23 @@
-<div id="head-wrap">
-    <a href="index" title="Go to main page"><img id="head-logo" src="assets/jaseph_normal.png"></a>
-    <div id="btn-holder">
-    <?php if(!isset($_COOKIE["logcheck"])):?>
-        <a href="login"><button id="login" class="btn">login</button></a>
-        <a href="register"><button id="register" class="btn">register</button></a>
+<div id='header'>
+    <a href='index' title='Go to main page'><img id='head-logo' src='assets/jaseph_normal.png'></a>
+    <div id='btn-holder'>
+
+    <?php if(!isset($_COOKIE['logcheck'])):?>
+        <a href='login'>
+          <button id='login' class='secondary-btn'>login</button>
+        </a>
+        <a href='register'>
+          <button id='register' class='primary-btn'>register</button>
+        </a>
+
     <?php else:?>
-        <a href="#"><button id="logout" class="btn" onclick="logout()">log out</button></a>
-        <a href="settings"><button id="nav-settings" class ="btn">settings</button></a>
+        <a href='#'>
+          <button id='logout' class='secondary-btn' onclick='logout()'>log out</button>
+        </a>
+        <a href='settings'>
+          <button id='nav-settings' class ='secondary-btn'>settings</button>
+        </a>
     <?php endif;?>
-        <img id="mask" src="assets/mask.png" onclick="swapStyle()" title="Change page theme">
+        <img id='mask' src='assets/mask.png' onclick='swapStyle()' title='Change page theme'>
     </div>
 </div>
