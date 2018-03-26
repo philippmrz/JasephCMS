f70fa74ea6b5ac1e9abb759fe9eafe9f9032e6a3
Philipp Merz <phmrch@gmail.com>
2018-03-27 00:40:36 +0200

    New Class DatabaseConnection now used in index

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
   <?php require 'require/head.php';?>
+
   <link rel="stylesheet" href="style/index.css" id="pagestyle">
 </head>
 <body>
@@ -16,51 +17,14 @@
   <?php require 'require/header.php';?>
   <div id='sidebar'></div>
   <div id='content'>
-    <div class='post'>
-      <img class='thumbnail' src='assets/dummy-thumbnail.png'>
-
-      <div class='post-without-tn'>
-        <div class='post-info'>
-          <p class='title'>dummy title text long one</p>
-          <div class='date-uname'>
-            <a class='username'>username</a>
-            <p class='at'>at</p>
-            <p class='date'>2018-12-12</p>
-          </div>
-        </div>
-        <p class='post-text'>lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem dolor ipsum sit amet sit dolor ipsum dolor sit dolor amet sit lorem. Lorem ipsum dolor sit amet sit amet dolor impsum alorem ipsum dolor sit amet lorem ipsum dolor sit</p>
-      </div>
-    </div>
-    <div class='post'>
-      <img class='thumbnail' src='assets/dummy-thumbnail.png'>
-
-      <div class='post-without-tn'>
-        <div class='post-info'>
-          <p class='title'>dummy title text long one</p>
-          <div class='date-uname'>
-            <a class='username'>username</a>
-            <p class='at'>at</p>
-            <p class='date'>2018-12-12</p>
-          </div>
-        </div>
-        <p class='post-text'>lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem dolor ipsum sit amet sit dolor ipsum dolor sit dolor amet sit lorem. Lorem ipsum dolor sit amet sit amet dolor impsum alorem ipsum dolor sit amet lorem ipsum dolor sit</p>
-      </div>
-    </div>
-    <div class='post'>
-      <img class='thumbnail' src='assets/dummy-thumbnail.png'>
-
-      <div class='post-without-tn'>
-        <div class='post-info'>
-          <p class='title'>dummy title text long one</p>
-          <div class='date-uname'>
-            <a class='username'>username</a>
-            <p class='at'>at</p>
-            <p class='date'>2018-12-12</p>
-          </div>
-        </div>
-        <p class='post-text'>lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem dolor ipsum sit amet sit dolor ipsum dolor sit dolor amet sit lorem. Lorem ipsum dolor sit amet sit amet dolor impsum alorem ipsum dolor sit amet lorem ipsum dolor sit</p>
-      </div>
-    </div>
+    <?php
+      //Include classes automatically
+      spl_autoload_register(function ($class) {
+        include 'require/' . $class . '.class.php';
+      });
+      $db = new DatabaseConnection();
+      echo $db->postsAusgeben();
+    ?>
   </div>
 </div>
 
---
+++ require/DatabaseConnection.class.php
@@ -0,0 +1,38 @@
+<?php
+  class DatabaseConnection extends mysqli {
+    function __construct() {
+        require('credentials.php');
+        @parent::__construct("$servername", "$username", "$password", "$dbname");
+
+          if ($this->connect_error) {
+            die($this->connect_errno . $mysqli->connect_error);
+          }
+    }
+
+    function postsAusgeben() {
+  require('credentials.php');
+      $r = @parent::query("SELECT TITLE, CONTENT, substring(DATE, 1, 10) AS DATE, USERNAME from $posttable, $usertable WHERE $posttable.USERID = $usertable.USERID ORDER BY DATE DESC");
+      while ($row = $r->fetch_assoc()){
+        $return .= <<<MYSQL
+<div class='post'>
+      <img class='thumbnail' src='assets/dummy-thumbnail.png'>
+
+      <div class='post-without-tn'>
+        <div class='post-info'>
+          <p class='title'>$row[TITLE]</p>
+          <div class='date-uname'>
+            <a class='username'>$row[USERNAME]</a>
+            <p class='at'>at</p>
+            <p class='date'>$row[DATE]</p>
+          </div>
+        </div>
+        <p class='post-text'>$row[CONTENT]</p>
+      </div>
+    </div>
+
+MYSQL;
+      }
+      return $return;
+    }
+  }
+?>
