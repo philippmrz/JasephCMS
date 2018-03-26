ded1247c35344525b4bab6aab6cf606675879bb0
Philipp Merz <phmrch@gmail.com>
2018-03-26 21:11:13 +0200

    Updated html & formatting

--- login.php
+++ login.php
@@ -1,11 +1,3 @@
-<!doctype html>
-<html>
-<head>
-  <?php require 'require/head.php';?>
-</head>
-<body>
-<?php require 'require/header.php';?>
-<script>applyStyle();</script>
 <?php
 // Credentials for this server
 require 'require/credentials.php';
@@ -14,7 +6,7 @@ $msg = array();
 $mysqli = new mysqli($servername, $username, $password, $dbname);
 
 if ($mysqli->connect_error) {
-    die("Connecting to MySQL or database failed:<b><i> ". $mysqli->connect_error . "</b></i>");
+    die("Connecting to MySQL or database failed:<b><i> " . $mysqli->connect_error . "</b></i>");
 }
 
 function random_string($length)
@@ -22,8 +14,8 @@ function random_string($length)
     $characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!$.";
     $c_length = strlen($characters);
     $string = "";
-    for ($i=0;$i<$length;$i++) {
-        $string .= $characters[rand(0, $c_length -1)];
+    for ($i = 0; $i < $length; $i++) {
+        $string .= $characters[rand(0, $c_length - 1)];
     }
     return $string;
 }
@@ -37,7 +29,7 @@ if (isset($_COOKIE["identifier"]) && isset($_COOKIE["token"])) {
     $db_token = $row["TOKEN"];
     $hash_token = hash("sha256", $token);
 
-    if ($hash_token == $db_token) {//new token
+    if ($hash_token == $db_token) { //new token
         $new_token = random_string(32);
         $hash_new_token = hash("sha256", $new_token);
         $update = $mysqli->query("UPDATE $usertable SET TOKEN = '$hash_new_token' WHERE IDENTIFIER = '$identifier'");
@@ -45,12 +37,12 @@ if (isset($_COOKIE["identifier"]) && isset($_COOKIE["token"])) {
         $row_u = $get_uname->fetch_assoc();
         $db_uname = $row_u["USERNAME"];
         ?><script>setCookie("identifier",$identifier);</script><?php
-        ?><script>setCookie("token",$new_token);</script><?php
-        ?><script>setCookie("logcheck","true");</script><?php
-        ?><script>setCookie("uname",$db_uname);</script>
+?><script>setCookie("token",$new_token);</script><?php
+?><script>setCookie("logcheck","true");</script><?php
+?><script>setCookie("uname",$db_uname);</script>
         <script>redirect("index");</script>
         <?php
-        exit;
+exit;
     } else {
         die("You're a cheater");
     }
@@ -66,21 +58,21 @@ if (isset($_POST["logbtn"])) {
         if ($result) {
             if (password_verify($pword, $db_p)) {
                 //pass
-                if (isset($_POST["stay_li"])) {//when checking "RM"
+                if (isset($_POST["stay_li"])) { //when checking "RM"
                     $identifier = random_string(32);
                     $token = random_string(32);
                     $hash_token = hash("sha256", $token);
                     $result = $mysqli->query("UPDATE $usertable SET IDENTIFIER = '$identifier', TOKEN = '$hash_token' WHERE USERNAME = '$uname'");
 
-                    ?><script>setCookie("identifier","<?php echo $identifier;?>");</script><?php
-                    ?><script>setCookie("token","<?php echo $token;?>");</script><?php
-                }
+                    ?><script>setCookie("identifier","<?php echo $identifier; ?>");</script><?php
+?><script>setCookie("token","<?php echo $token; ?>");</script><?php
+}
                 ?><script>setCookie("logcheck","true");</script><?php
-                ?><script>setCookie("uname","<?php echo $uname;?>");</script>
+?><script>setCookie("uname","<?php echo $uname; ?>");</script>
 
                 <script>redirect("index");</script>
                 <?php
-                exit;
+exit;
             } else {
                 //invalid
                 array_push($msg, "Invalid password or username");
@@ -95,30 +87,51 @@ if (isset($_POST["logbtn"])) {
 
 $mysqli->close();
 ?>
-<div id="auth">
-<p id="title">Login</p>
-<form method="POST" action="">
-  <?php
-  if (isset($uname) && !empty($uname)) {
-      echo '<input id="username" type="text" name="uname" placeholder="Username" value="'. $uname .'"/><br>';
-  } else {
-      echo '<input id="username" type="text" name="uname" placeholder="Username"/><br>';
-  }
-  ?>
-  <input class="password" type="password" name="pword" placeholder="Password"/><br>
-  <input id="authbtn" type="submit" name="logbtn" value="Sign In"/><br>
-  Remember me:<input type="checkbox" name="stay_li"/><br>
-<?php
 
+<!doctype html>
+<html>
+<head>
+  <?php require 'require/head.php';?>
+  <link rel="stylesheet" href="style/login.css" id="pagestyle">
+</head>
+<body>
+<div id='grid-wrap'>
+  <?php require 'require/header.php';?>
+  <div id='sidebar'></div>
+  <div id='content'>
+    <div id="auth">
+    <p id="title">Login</p>
+      <form method="POST" action="">
+        <?php
+if (isset($uname) && !empty($uname)) {
+    echo '<input id="username" type="text" name="uname" placeholder="Username" value="' . $uname . '"/><br>';
+} else {
+    echo '<input id="username" type="text" name="uname" placeholder="Username"/><br>';
+}
+?>
+
+        <input class="password" type="password" name="pword" placeholder="Password"/><br>
+        <input id="authbtn" type="submit" name="logbtn" value="Sign In"/><br>
+        <p id='remember-me'>Remember me:</p>
+        <input type="checkbox" name="stay_li"/><br>
+
+        <?php
 if (!empty($msg)) {
     foreach ($msg as $text) {
         echo "<font size=\"2px\" color=\"red\"><i>" . $text . "</font></i><br>";
     }
 }
 ?>
-<p id="sub">No Account yet? Click <a href="register.php">here</a> to register</p>
 
-</form>
+        <p id="sub">No Account yet? Click <a href="register.php">here</a> to register</p>
+
+      </form>
+    </div>
+
+ </div>
 </div>
+
+<script>applyStyle();</script>
+
 </body>
 </html>
