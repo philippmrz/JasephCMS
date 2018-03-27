7ebcb6bdecd2b2ca4426b9a9f9611ab79a87f3a4
Philipp Merz <phmrch@gmail.com>
2018-03-27 14:32:28 +0200

    Created & added links and files for sidebar

--- require/sidebar.php
+++ require/sidebar.php
@@ -1,7 +1,17 @@
 <div id='sidebar'>
-  <div class='sidebar-elem' id='front-page'><a>frontpage</a></div>
-  <div class='sidebar-elem' id='saved'><a>saved posts</a></div>
-  <div class='sidebar-elem' id='my-posts'><a>my posts</a></div>
-  <div class='sidebar-elem' id='settings'><a>settings</a></div>
-  <div class='sidebar-elem' id='new-post'><a>new post</a></div>
+  <div class='sidebar-elem' id='front-page'>
+    <a href='index'>frontpage</a>
+  </div>
+  <div class='sidebar-elem' id='saved'>
+    <a href='saved'>saved posts</a>
+  </div>
+  <div class='sidebar-elem' id='my-posts'>
+    <a href='myposts'>my posts</a>
+  </div>
+  <div class='sidebar-elem' id='settings'>
+    <a href='settings'>settings</a>
+  </div>
+  <div class='sidebar-elem' id='new-post'>
+    <a href='newpost'>new post</a>
+  </div>
 </div>
