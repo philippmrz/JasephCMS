a71601a2da484000d168f0105f43d15dc4f09386
Philipp Merz <phmrch@gmail.com>
2018-03-27 14:21:58 +0200

    First sidebar mockup

---
+++ require/sidebar.php
@@ -0,0 +1,7 @@
+<div id='sidebar'>
+  <div class='sidebar-elem' id='front-page'><a>frontpage</a></div>
+  <div class='sidebar-elem' id='saved'><a>saved posts</a></div>
+  <div class='sidebar-elem' id='my-posts'><a>my posts</a></div>
+  <div class='sidebar-elem' id='settings'><a>settings</a></div>
+  <div class='sidebar-elem' id='new-post'><a>new post</a></div>
+</div>
