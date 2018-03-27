af6a283ab3421321cd6529e9b3933b2a4c3d0abf
Philipp Merz <phmrch@gmail.com>
2018-03-27 19:53:24 +0200

    Added frame for new pages

---
+++ myposts.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+  <?php require 'require/head.php';?>
+  <link rel="stylesheet" href="style/myposts.css" id="pagestyle">
+</head>
+<body>
+
+<div id='grid-wrap'>
+  <?php require 'require/header.php';?>
+  <?php require 'require/sidebar.php'; ?>
+  <div id='content'>
+  </div>
+</div>
+
+<script>applyStyle();</script>
+
+</body>
+</html>
