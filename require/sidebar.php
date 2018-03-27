05403b078b9ffad9575b31062bcd66e450164dd6
Philipp Merz <phmrch@gmail.com>
2018-03-27 15:12:53 +0200

    Finished sidebar

--- require/sidebar.php
+++ require/sidebar.php
@@ -1,17 +1,22 @@
 <div id='sidebar'>
-  <div class='sidebar-elem' id='front-page'>
-    <a href='index'>frontpage</a>
-  </div>
-  <div class='sidebar-elem' id='saved'>
-    <a href='saved'>saved posts</a>
-  </div>
-  <div class='sidebar-elem' id='my-posts'>
-    <a href='myposts'>my posts</a>
-  </div>
-  <div class='sidebar-elem' id='settings'>
-    <a href='settings'>settings</a>
-  </div>
-  <div class='sidebar-elem' id='new-post'>
-    <a href='newpost'>new post</a>
-  </div>
+  <a class='sidebar-elem' href='index' id='front-page'
+      <?= ($_SERVER['REQUEST_URI'] == "/") ? "status='active'" : ""?>>
+      frontpage
+  </a>
+  <a class='sidebar-elem' href='saved' id='saved'
+      <?= ($_SERVER['REQUEST_URI'] == "/saved") ? "status='active'" : ""?>>
+      saved posts
+  </a>
+  <a class='sidebar-elem' href='myposts' id='my-posts'
+      <?= ($_SERVER['REQUEST_URI'] == "/myposts") ? "status='active'" : ""?>>
+      my posts
+  </a>
+  <a class='sidebar-elem' href='settings' id='settings'
+      <?= ($_SERVER['REQUEST_URI'] == "/settings") ? "status='active'" : ""?>>
+      settings
+  </a>
+  <a class='sidebar-elem' href='newpost' id='new-post'
+      <?= ($_SERVER['REQUEST_URI'] == "/newpost") ? "status='active'" : ""?>>
+      new post
+  </a>
 </div>
