942cd6b4c7f6a4b48dfb007732a67a480c266baf
Philipp Merz <phmrch@gmail.com>
2018-03-27 14:21:44 +0200

    Added new font

--- require/head.php
+++ require/head.php
@@ -1,6 +1,6 @@
 <link rel="icon" href="assets/icon.png"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="https://fonts.googleapis.com/css?family=Lato|Slabo+27px" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans|Lato|Slabo+27px" rel="stylesheet">
 <link rel="stylesheet" href="style/general.css" id="pagestyle">
 <script src="script/script.js"></script>
 <title>JasephCMS</title>
