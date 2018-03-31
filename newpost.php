60193cc1f40551b5dc5c4cfd2257a5ab2b4cec5b
Philipp Merz <phmrch@gmail.com>
2018-03-31 22:04:31 +0200

    Applied new structure for redesign

--- newpost.php
+++ newpost.php
@@ -1,81 +1,65 @@
+<?php if (!isset($_COOKIE["logcheck"])):
+  header('Location: index');
+
+elseif (isset($_POST["title"])):
+  require 'require/credentials.php';
+  $mysqli = new mysqli("$servername", "$username", "$password", "$dbname");
+
+  if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+  }
+
+  $username = $_COOKIE["uname"];
+  $mysqliUserID = $mysqli->query("SELECT USERID FROM $usertable WHERE USERNAME = '$username'");
+  if (!$mysqliUserID) {
+      echo $mysqli->error;
+  }
+
+  $row_uid = $mysqliUserID->fetch_assoc();
+  $userID= $row_uid["USERID"];
+
+  $title = $mysqli->escape_string($_POST["title"]);
+  $content = $mysqli->escape_string($_POST["content"]);
+
+  $insert = $mysqli->query("INSERT INTO $posttable (USERID, TITLE, CONTENT) VALUES ('$userID', '$title', '$content')");
+  if (!$insert) {
+      echo $mysqli->error;
+  }
+
+  $mysqli->close();
+  header("Location: index");
+
+endif; ?>
+
 <!doctype html>
 <html>
 <head>
   <?php require 'require/head.php';?>
+  <link rel="stylesheet" href="style/myposts.css" id="pagestyle">
 </head>
 <body>
+
+<div id='grid-wrap'>
   <?php require 'require/header.php';?>
-  <script>applyStyle();</script>
-  <div class="content">
-    <?php if (isset($_COOKIE["logcheck"])): ?>
-      <?php if (!isset($_POST["title"])): ?>
-        <h1>New Post</h1>
-        <form action="" method="POST">
-          <div id="titlecharswrapper">0/200</div>
-          <input id="titleField" name="title" type="text" placeholder="Post title" autocomplete="" maxlength="200" oninput="updateCharsLeft(200, 'title')" required/><br><br>
-          <div id="contentcharswrapper">0/10000</div>
-          <textarea id="contentArea" name="content" placeholder="Post content" rows="20" cols="100" spellcheck="false" maxlength="10000" oninput="updateCharsLeft(10000, 'content')" required></textarea><br>
-          <div id="btnpostwrapper">
-            <button type="submit" id="btnpost" class="btn">Post</button>
-          </div>
-        </form>
-      <?php else: ?>
-        <?php
-// Credentials for this server
-require 'require/credentials.php';
+  <?php require 'require/sidebar.php'; ?>
+  <div id='content'>
 
-$success = true;
+    <form action="" method="POST">
+      <p>New Post</p>
 
-if (!isset($_POST["title"]) || empty($_POST["title"]) || ctype_space($_POST["title"])) { //Check if the title is not set/empty/only spaces. Exit if true.
-    echo 'Something went wrong. Title must not be empty! For some reason submitting an empty input was allowed.<br>';
-    $success = false;
-}
-
-if (!isset($_POST["content"]) || empty($_POST["content"]) || ctype_space($_POST["content"])) { //Check if the content is not set/empty/only spaces. Exit if true.
-    echo 'Something went wrong. Content must not be empty! For some reason submitting an empty input was allowed.<br>';
-    $success = false;
-}
-
-$mysqli = new mysqli("$servername", "$username", "$password", "$dbname");
-
-if ($mysqli->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-}
-
-if ($success) {
-    $uname = $_COOKIE["uname"];
-    $get_uid = $mysqli->query("SELECT USERID FROM $usertable WHERE USERNAME = '$uname'");
-    if (!$get_uid) {
-        echo $mysqli->error;
-    }
-    $row_uid = $get_uid->fetch_assoc();
-    $db_uid = $row_uid["USERID"];
-
-    $title = $mysqli->escape_string($_POST["title"]);
-    $content = $mysqli->escape_string($_POST["content"]);
-
-    $insert = $mysqli->query("INSERT INTO $posttable (USERID, TITLE, CONTENT) VALUES ('$db_uid', '$title', '$content')"); // Inserts data into the post table
-    if (!$insert) {
-        echo $mysqli->error;
-    }
-
-    $mysqli->close();
-    ?>
-            <h1>Successfully created new post!</h1>
-            <script>redirect("index", 5);</script>
-            <?php
-} else {
-    ?>
-            <h1>Creation of new post failed!</h1>
-            <?php
-}
-?>
-      <?php endif;?>
-    <?php else: ?>
-    <h1>Get Out</h1>
-    <script>redirect('index');</script>
-    <?php endif;?>
+      <div id="titlecharswrapper">0/200</div>
+      <input id="titleField" name="title" type="text" placeholder="Post title" autocomplete="" maxlength="200" oninput="updateCharsLeft(200, 'title')" required/><br><br>
+      <div id="contentcharswrapper">0/10000</div>
+      <textarea id="contentArea" name="content" placeholder="Post content" rows="20" cols="100" spellcheck="false" maxlength="10000" oninput="updateCharsLeft(10000, 'content')" required></textarea><br>
+      <div id="btnpostwrapper">
+      <button type="submit" id="btnpost" class="btn">Post</button>
+      </div>
+      </form>
   </div>
+</div>
+
+<script>applyStyle();</script>
+
 </body>
 </html>
