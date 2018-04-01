1268c49904898e319ad01dd55434e2ef2a8ad8b6
Philipp Merz <phmrch@gmail.com>
2018-04-01 18:00:39 +0200

    Added jumbotron advertising register if not logged in

--- index.php
+++ index.php
@@ -11,6 +11,13 @@
   <?php require 'require/header.php';?>
   <?php require 'require/sidebar.php'; ?>
   <div id='content'>
+    <?php if (!isset($_COOKIE['logcheck'])): ?>
+      <div id='jumbotron'>
+        <p>Register to start blogging and to access more features.</p>
+        <a id='jumbotron-btn' href='register' class='primary-btn'>register</a>
+      </div>
+    <?php endif; ?>
+
     <?php
       $db = new DatabaseConnection();
       echo $db->postsAusgeben();
