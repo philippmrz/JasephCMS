81ddcb559fbb20337092e1294f0b2b4a55b9dd3e
Kaeks <Kaeks@users.noreply.github.com>
2018-04-09 17:28:11 +0200

    Added header and sidebar, made code formatting readable

--- userlist.php
+++ userlist.php
@@ -12,70 +12,73 @@ $uname = $_COOKIE["uname"];
 
 //check for admin/mod
 $get_admin = $mysqli->query("SELECT ROLE FROM $usertable WHERE USERNAME = '$uname'");
-if(!$get_admin) {
+if (!$get_admin) {
     echo $mysqli->error;
 }
 $row_admin = $get_admin->fetch_assoc();
-if($row_admin["ROLE"] == "COMMON" || $row_admin["ROLE"] == "BLOCKED" || !isset($_COOKIE["logcheck"])){?>
+if ($row_admin["ROLE"] == "COMMON" || $row_admin["ROLE"] == "BLOCKED" || !isset($_COOKIE["logcheck"])) {
+  ?>
   <div class="content">
     <h1>Get Out</h1>
     <script>redirect('index');</script>
   </div>
-<?php }else if($row_admin["ROLE"] == "ADMIN"){$admin = true;}
-      else{$admin = false;}
+  <?php
+} elseif ($row_admin["ROLE"] == "ADMIN") {
+    $admin = true;
+} else {
+    $admin = false;
+}
 
-if(isset($_POST["canclbtn"])){ ?>
-  <script>redirect("settings.php")</script>
-<?php }
-if(isset($_POST["confbtn"])){
+if (isset($_POST["canclbtn"])) {
+    ?><script>redirect("settings.php")</script><?php
+}
+if (isset($_POST["confbtn"])) {
   $msg = [];
   $userlist = $mysqli->query("SELECT USERNAME FROM $usertable");
-  while($row = $userlist->fetch_assoc()){
-  //check every user
-  foreach($row as $user){
-	$getUserID = $mysqli->query("SELECT USERID FROM $usertable WHERE USERNAME = '$user'");
-	$idRow = $getUserID->fetch_assoc();
-	$id = $idRow["USERID"];
-    if(isset($_POST["chkusers$id"])){
-      switch($_POST["chkusers$id"]){
-        case "admin":
-          $new_role = "ADMIN";
-          break;
+  while ($row = $userlist->fetch_assoc()) {
+    //check every user
+    foreach ($row as $user) {
+      $getUserID = $mysqli->query("SELECT USERID FROM $usertable WHERE USERNAME = '$user'");
+      $idRow = $getUserID->fetch_assoc();
+      $id = $idRow["USERID"];
+      if (isset($_POST["chkusers$id"])) {
+        switch ($_POST["chkusers$id"]) {
+          case "admin":
+            $new_role = "ADMIN";
+            break;
 
-        case "mod":
-          $new_role = "MOD";
-          break;
+          case "mod":
+            $new_role = "MOD";
+            break;
 
-        case "block":
-          $new_role = "BLOCKED";
-          break;
+          case "block":
+            $new_role = "BLOCKED";
+            break;
 
-        case "delete":
-          $delete = $mysqli->query("DELETE FROM $usertable WHERE USERID = $id");
-          if($delete){
-            array_push($msg,"Successfully deleted User no.$id");
-          }
-          else{
-            array_push($msg,"Couldn't carry out changes (db query failed)");
-          }
-          break;
-      }
-      //change table, get re:message
-      if(isset($new_role)){
-        $role = $mysqli->query("UPDATE $usertable SET ROLE = '$new_role' WHERE USERID = $id");
-        $get_uname = $mysqli->query("SELECT USERNAME FROM $usertable WHERE USERID = $id");
-        $urow = $get_uname->fetch_assoc();
-        $uname = $urow["USERNAME"];
-        if($role && $get_uname){
-          array_push($msg,"Changed Role of $uname to $new_role");
+          case "delete":
+            $delete = $mysqli->query("DELETE FROM $usertable WHERE USERID = $id");
+            if ($delete) {
+              array_push($msg, "Successfully deleted User no.$id");
+            } else {
+              array_push($msg, "Couldn't carry out changes (db query failed)");
+            }
+            break;
         }
-        else{
-          array_push($msg,"Couldn't carry out changes (db query failed)");
+        //change table, get re:message
+        if (isset($new_role)) {
+          $role = $mysqli->query("UPDATE $usertable SET ROLE = '$new_role' WHERE USERID = $id");
+          $get_uname = $mysqli->query("SELECT USERNAME FROM $usertable WHERE USERID = $id");
+          $urow = $get_uname->fetch_assoc();
+          $uname = $urow["USERNAME"];
+          if ($role && $get_uname) {
+            array_push($msg, "Changed Role of $uname to $new_role");
+          } else {
+            array_push($msg, "Couldn't carry out changes (db query failed)");
+          }
         }
       }
-	 }
-   }
- }
+    }
+  }
 }
 ?>
 
@@ -87,57 +90,79 @@ if(isset($_POST["confbtn"])){
 </head>
 
 <body>
-
-<?php require 'require/header.php';?>
-<script>applyStyle();</script>
-<?php if(!empty($msg)){
-  echo "Status: <br>";
-  echo "-------------<br>";
-  foreach($msg as $text){
-    echo $text."<br>";
-  }
-  echo "-------------<br>";
-}?>
-<form method="POST" action="">
-<?php
-$result= $mysqli->query("SELECT USERID, USERNAME, REGISTERED, ROLE FROM $usertable");
-$fl = true;
-if($result){
-  while($row = $result->fetch_assoc()){
-    if($fl){
-      $fl = false;
-      $heads = array_keys($row);
-      ?><table border=3><tr><?php
-      foreach($heads as $elem){
-        echo "<th>".$elem."</th>";
+  <div id='grid-wrap'>
+    <?php require 'require/header.php';?>
+    <?php require 'require/sidebar.php';?>
+    <div id='content'>
+      <script>applyStyle();</script>
+      <?php
+      if (!empty($msg)) {
+        echo "Status: <br>";
+        echo "-------------<br>";
+        foreach ($msg as $text) {
+          echo $text."<br>";
+        }
+        echo "-------------<br>";
       }
-      ?><th>Delete</th><th>Block</th>
-      <?php if($admin){echo"<th>Mod</th><th>Admin</th>";}?>
-    </tr><?php
-  }?><tr><?php
-    foreach($row as $elem){
-      echo"<td>".$elem."</td>";
-    }
-    ?><td><input type="radio" name="chkusers<?php echo $row["USERID"]."\"";?> value="delete"/></td>
-	  <td><input type="radio" name="chkusers<?php echo $row["USERID"]."\"";?> value="block"/></td>
-	  <?php if($admin):?>
-    <td><input type="radio" name="chkusers<?php echo $row["USERID"]."\"";?> value="mod"/></td>
-	  <td><input type="radio" name="chkusers<?php echo $row["USERID"]."\"";?> value="admin"/></td>
-    <?php endif; ?></tr><?php
-  }
-}
-else{echo "nah";}
-
-?></table><br>
-<input type="submit" name="confbtn" value="Confirm"/></br></br>
-<input type="submit" name="canclbtn" value="Cancel"/>
-
-</form><?php
-
-$mysqli->close();
- ?>
-
-
-<?php//require 'require/footer.php';?>
+      ?>
+      <form method="POST" action="">
+        <?php
+        $result= $mysqli->query("SELECT USERID, USERNAME, REGISTERED, ROLE FROM $usertable");
+        $fl = true;
+        if ($result) {
+          while ($row = $result->fetch_assoc()) {
+            if ($fl) {
+              $fl = false;
+              $heads = array_keys($row);
+              ?>
+              <table border=3>
+                <tr>
+                  <?php
+                  foreach ($heads as $elem) {
+                    echo "<th>".$elem."</th>";
+                  }
+                  ?>
+                  <th>Delete</th><th>Block</th>
+                  <?php
+                  if ($admin) {
+                    echo "<th>Mod</th><th>Admin</th>";
+                  }
+                  ?>
+                </tr>
+                <?php
+              }
+              ?>
+              <tr>
+                <?php
+                foreach ($row as $elem) {
+                  echo"<td>".$elem."</td>";
+                }
+                ?>
+                <td><input type="radio" name="chkusers<?php echo $row["USERID"]; ?>" value="delete"/></td>
+                <td><input type="radio" name="chkusers<?php echo $row["USERID"]; ?>" value="block"/></td>
+                <?php
+                if ($admin):
+                  ?>
+                  <td><input type="radio" name="chkusers<?php echo $row["USERID"]; ?>" value="mod"/></td>
+                  <td><input type="radio" name="chkusers<?php echo $row["USERID"]; ?>" value="admin"/></td>
+                  <?php
+                endif;
+                ?>
+              </tr>
+              <?php
+            }
+          } else {
+            echo "nah";
+          }
+          ?>
+        </table>
+        <br>
+        <input type="submit" name="confbtn" value="Confirm"/>
+        <br>
+        <input type="submit" name="canclbtn" value="Cancel"/>
+      </form>
+    </div>
+</div>
+<script>applyStyle();</script>
 </body>
 </html>
