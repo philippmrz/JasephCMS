e83bc962b8a3a3e7af875582ff3a3926e85dbfc2
Zaby <32384332+Zaby0@users.noreply.github.com>
2018-04-09 20:52:05 +0200

    does what it should (details in desc)
    gibt die posts vom user aus, solange myposts.css noch nicht existiert hab ichs durch index.css ersetzt kann man dann ja später changen so siehts erstmal einheitlich aus

--- myposts.php
+++ myposts.php
@@ -1,8 +1,18 @@
+<?php
+// Credentials for this server
+require 'require/credentials.php';
+
+$mysqli = new mysqli($servername, $username, $password, $dbname);
+
+if ($mysqli->connect_error) {
+    die("Connecting to MySQL or database failed:<b><i> " . $mysqli->connect_error . "</b></i>");
+}?>
+
 <!doctype html>
 <html>
 <head>
   <?php require 'require/head.php';?>
-  <link rel="stylesheet" href="style/myposts.css" id="pagestyle">
+  <link rel="stylesheet" href="style/index.css" id="pagestyle">
 </head>
 <body>
 
@@ -10,6 +20,19 @@
   <?php require 'require/header.php';?>
   <?php require 'require/sidebar.php'; ?>
   <div id='content'>
+    <?php
+      $uname = $_COOKIE["uname"];
+      $getUserID = $mysqli->query("SELECT USERID FROM $usertable WHERE USERNAME = '$uname'");
+      $row = $getUserID->fetch_assoc();
+      $userID = $row["USERID"];
+      $db = new DatabaseConnection();
+      echo $db->postsAusgeben($_GET['sort'],$userID);
+
+    if(isLoggedIn()): ?>
+      <a title='Sort chronologically or reverse chronologically' class='floating-action-btn' href='index.php?sort=<?= invertSortOrder()?>'>
+        <?= getSortSVG() ?>
+      </a>
+    <?php endif; ?>
   </div>
 </div>
 
