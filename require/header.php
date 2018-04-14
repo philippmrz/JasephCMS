27665a62b466258a7d7284711667d474017f286c
Kaeks <Kaeks@users.noreply.github.com>
2018-04-14 15:49:41 +0200

    Set header image src to empty to prevent occasional flashes on mobile

--- require/header.php
+++ require/header.php
@@ -1,5 +1,5 @@
 <div id='header'>
-  <a id='logo-wrapper' href='index' title='Go to main page'><img id='head-logo' src='assets/jaseph_normal.png'></a>
+  <a id='logo-wrapper' href='index' title='Go to main page'><img id='head-logo'></a>
   <div id='btn-holder'>
     <?php if(!isLoggedIn()):?>
     <a href='login'>
