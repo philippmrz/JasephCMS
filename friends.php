5c73a0c212a9579a7e88e0cfc389f15180e60ca4
Kaeks <Kaeks@users.noreply.github.com>
2018-05-03 21:03:48 +0200

    Redesign sidebar, add (wip) friends and users pages

---
+++ friends.php
@@ -0,0 +1,28 @@
+<?php
+require_once('require/backend.php');
+require('require/credentials.php');
+$db = new DatabaseConnection();
+?>
+
+<!doctype html>
+<html>
+<head>
+  <?php require 'require/head.php';?>
+  <script>applyStyle();</script>
+  <link rel="stylesheet" href="style/friends.css">
+</head>
+<body>
+
+<div id='grid-wrap'>
+  <?php require 'require/header.php';?>
+  <?php require 'require/sidebar.php'; ?>
+  <div id='content'>
+
+  </div>
+</div>
+
+<script>
+  applyStyle();
+</script>
+</body>
+</html>
--- require/sidebar.php
+++ require/sidebar.php
@@ -4,60 +4,66 @@ if ($db->auth()):
   $userid = $db->getCurUser();
 ?>
 <div id='sidebar'>
-  <a class='sidebar-elem' href='index' id='front-page'
-    <?= (strpos($_SERVER['REQUEST_URI'], '/index') !== false or $_SERVER['REQUEST_URI'] == '' or $_SERVER['REQUEST_URI'] == '/') ? "status='active'" : ""?>>
-    frontpage
+  <a class='sidebar-elem' href='index' id='front-page' <?= $db->getActive('index') ? "status='active'" : ""?>>
+    <svg viewBox="0 0 24 24"><path d='<?= getSVG('home');?>'/></svg>
+    <span>frontpage</span>
   </a>
-  <a class='sidebar-elem' href='saved' id='saved'
-    <?= strpos($_SERVER['REQUEST_URI'], '/saved') !== false ? "status='active'" : ""?>>
-    saved posts
+  <a class='sidebar-elem' href='saved' id='saved' <?= $db->getActive('saved') ? "status='active'" : ""?>>
+    <svg viewBox="0 0 24 24"><path d='<?= getSVG('saved');?>'/></svg>
+    <span>saved posts</span>
   </a>
-  <a class='sidebar-elem' href='profile?id=<?=$userid?>' id='profile'
-    <?= strpos($_SERVER['REQUEST_URI'], "/profile?id=$userid") !== false ? "status='active'" : ""?>>
-    my profile
+  <a class='sidebar-elem' href='profile?id=<?=$userid?>' id='profile' <?= $db->getActive('myprofile') ? "status='active'" : ""?>>
+    <svg viewBox="0 0 24 24"><path d='<?= getSVG('profile');?>'/></svg>
+    <span>my profile</span>
   </a>
-  <a class='sidebar-elem' href='drafts' id='drafts'
-    <?= strpos($_SERVER['REQUEST_URI'], '/drafts') !== false ? "status='active'" : ""?>>
-    my drafts
+  <a class='sidebar-elem' href='drafts' id='drafts' <?= $db->getActive('drafts') ? "status='active'" : ""?>>
+    <svg viewBox="0 0 24 24"><path d='<?= getSVG('drafts');?>'/></svg>
+    <span>my drafts</span>
   </a>
-  <a class='sidebar-elem' href='newpost' id='new-post'
-    <?= strpos($_SERVER['REQUEST_URI'], '/newpost') !== false ? "status='active'" : ""?>>
-    new post
+  <a class='sidebar-elem' href='newpost' id='new-post' <?= $db->getActive('newpost') ? "status='active'" : ""?>>
+    <svg viewBox="0 0 24 24"><path d='<?= getSVG('newpost');?>'/></svg>
+    <span>new post</span>
   </a>
+  <div class='category'>
+    Community
+  </div>
+  <a class='sidebar-elem' href='users' id='users' <?= $db->getActive('users') ? "status='active'" : ""?>>
+    <svg viewBox="0 0 24 24"><path d='<?= getSVG('users');?>'/></svg>
+    <span>users</span>
+  </a>
+  <a class='sidebar-elem' href='friends' id='friends' <?= $db->getActive('friends') ? "status='active'" : ""?>>
+    <svg viewBox="0 0 24 24"><path d='<?= getSVG('friends');?>'/></svg>
+    <span>friends</span>
+  </a>
+  <div id='following'>
+  </div>
 </div>
 <?php endif; ?>
 
 <!-- Mobile only-->
 <div id='navbar'>
-  <a class='navbar-elem' href='index'
-    <?= (strpos($_SERVER['REQUEST_URI'], '/index') !== false or $_SERVER['REQUEST_URI'] == '' or $_SERVER['REQUEST_URI'] == '/') ? "status='active'" : ""?>>
-    <svg class='svg-24' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d='<?php echo getSVG('home');?>'/></svg>
+  <a class='navbar-elem' href='index' <?= $db->getActive('index') ? "status='active'" : ""?>>
+    <svg class='svg-24' viewBox="0 0 24 24"><path d='<?= getSVG('home');?>'/></svg>
   </a>
   <?php if ($db->auth()):?>
-    <a class='navbar-elem' href='saved'
-      <?= strpos($_SERVER['REQUEST_URI'], '/saved') !== false ? "status='active'" : ""?>>
-      <svg class='svg-24' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d='<?= getSVG('saved');?>'/></svg>
+    <a class='navbar-elem' href='saved'<?= $db->getActive('saved') ? "status='active'" : ""?>>
+      <svg class='svg-24' viewBox="0 0 24 24"><path d='<?= getSVG('saved');?>'/></svg>
     </a>
-    <a class='navbar-elem' href='profile?id=<?=$userid?>'
-      <?= strpos($_SERVER['REQUEST_URI'], "/profile?id=$userid") !== false ? "status='active'" : ""?>>
-      <svg class='svg-24' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d='<?= getSVG('profile');?>'/></svg>
+    <a class='navbar-elem' href='profile?id=<?=$userid?>' <?= $db->getActive('myprofile') ? "status='active'" : ""?>>
+      <svg class='svg-24' viewBox="0 0 24 24"><path d='<?= getSVG('profile');?>'/></svg>
     </a>
-    <a class='navbar-elem' href='drafts'
-      <?= strpos($_SERVER['REQUEST_URI'], '/drafts') !== false ? "status='active'" : ""?>>
-      <svg class='svg-24' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d='<?= getSVG('drafts');?>'/></svg>
+    <a class='navbar-elem' href='drafts' <?= $db->getActive('drafts') ? "status='active'" : ""?>>
+      <svg class='svg-24' viewBox="0 0 24 24"><path d='<?= getSVG('drafts');?>'/></svg>
     </a>
-    <a class='navbar-elem' href='newpost'
-      <?= strpos($_SERVER['REQUEST_URI'], '/newpost') !== false ? "status='active'" : ""?>>
-      <svg class='svg-24' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d='<?= getSVG('newpost');?>'/></svg>
+    <a class='navbar-elem' href='newpost' <?= $db->getActive('newpost') ? "status='active'" : ""?>>
+      <svg class='svg-24' viewBox="0 0 24 24"><path d='<?= getSVG('newpost');?>'/></svg>
     </a>
   <?php else:?>
-    <a class='navbar-elem' href='login'
-      <?= strpos($_SERVER['REQUEST_URI'], '/login') !== false ? "status='active'" : ""?>>
-      <svg class='svg-24' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d='<?= getSVG('login');?>'/></svg>
+    <a class='navbar-elem' href='login' <?= $db->getActive('login') ? "status='active'" : ""?>>
+      <svg class='svg-24' viewBox="0 0 24 24"><path d='<?= getSVG('login');?>'/></svg>
     </a>
-    <a class='navbar-elem' href='register'
-      <?= (strpos($_SERVER['REQUEST_URI'], "/register")) ? "status='active'" : ""?>>
-      <svg class='svg-24' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d='<?= getSVG('register');?>'/></svg>
+    <a class='navbar-elem' href='register' <?= $db->getActive('register') ? "status='active'" : ""?>>
+      <svg class='svg-24' viewBox="0 0 24 24"><path d='<?= getSVG('register');?>'/></svg>
     </a>
   <?php endif;?>
 </div>
