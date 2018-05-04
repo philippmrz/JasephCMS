b878f811c1be1bb835eb119d905241138fd1c05f
Kaeks <Kaeks@users.noreply.github.com>
2018-05-04 22:28:31 +0200

    Remove unused js cookie functions

--- script/general.js
+++ script/general.js
@@ -5,58 +5,6 @@
  *
  */
 
-function setCookie(cname, val, time) { // for time use new Date(years, months, days, hours, minutes, seconds, milliseconds);
-    var now = new Date();
-    if (time != null) {
-        var expiry = new Date(now.getTime() + time);
-        document.cookie = cname + "=" + val + "; expires=" + expiry.toUTCString() + "; path=";
-    } else {
-        document.cookie = cname + "=" + val + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=";
-    }
-    //DEBUG console.log("Set value of cookie '" + cname + "' to '" + val + "'");
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return null;
-}
-
-function listCookies() {
-    var theCookies = document.cookie.split(';');
-    var aString = '';
-    for (var i = 1 ; i <= theCookies.length; i++) {
-        aString += i + ' ' + theCookies[i-1] + "\n";
-    }
-    return aString;
-}
-
-function deleteCookie(cname) {
-  document.cookie = cname + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT; path=';
-}
-
-function deleteAllCookies() {
-    var cookies = document.cookie.split(";");
-
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
-        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }
-    //DEBUG console.log("Deleted all cookies.");
-}
-
 function logout() {
     deleteCookie("identifier");
     deleteCookie("hashed_password");
